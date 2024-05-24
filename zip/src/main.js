15ce1e4a5f36a4b6575ceb47bc308bacacaafc24
Ian Yates <ian@snaptin.com>
2024-05-24 11:23:15 +0200

    Add files via upload

---
+++ zip/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/5jA9A3n8-EuuL3K7/scene.splinecode');
